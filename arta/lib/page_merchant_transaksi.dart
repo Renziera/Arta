de401f44c405200fc284971553e68ebcefeaefe2
Stanley Heryanto <stanleyheryanto@Gmail.com>
2019-09-04 00:41:07 +0700

    Merchant Wallet & Transaksi

--- arta/lib/page_merchant_transaksi.dart
+++ arta/lib/page_merchant_transaksi.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class MerchantTransaksi extends StatefulWidget {
@@ -6,12 +8,51 @@ class MerchantTransaksi extends StatefulWidget {
 }
 
 class _MerchantTransaksiState extends State<MerchantTransaksi> {
+  List<DocumentSnapshot> _list = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  void fetchData() async {
+    FirebaseUser user = await FirebaseAuth.instance.currentUser();
+    Firestore.instance
+        .collection('pengguna')
+        .document(user.uid)
+        .collection('transaksi')
+        .orderBy('waktu', descending: true)
+        .snapshots()
+        .listen((querySnapshot) {
+      setState(() {
+        _list = querySnapshot.documents;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Text('Transaksi'),
-      ],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Transaksi',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            ListView(
+              shrinkWrap: true,
+              children: _list.map((ds) {
+                return ListTile(
+                  title: Text(ds.documentID),
+                );
+              }).toList(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- arta/lib/page_merchant_wallet.dart
+++ arta/lib/page_merchant_wallet.dart
@@ -1,3 +1,6 @@
+import 'package:Arta/util.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class MerchantWallet extends StatefulWidget {
@@ -6,12 +9,117 @@ class MerchantWallet extends StatefulWidget {
 }
 
 class _MerchantWalletState extends State<MerchantWallet> {
+  String saldo;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  void fetchData() async {
+    FirebaseUser user = await FirebaseAuth.instance.currentUser();
+    Firestore.instance
+        .collection('pengguna')
+        .document(user.uid)
+        .snapshots()
+        .listen((ds) {
+      setState(() {
+        saldo = ds.data['saldo'].toString();
+        saldo = formatUang(saldo);
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Text('Wallet'),
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: <Widget>[
+          Text(
+            'Wallet Anda',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          Card(
+            margin: EdgeInsets.symmetric(horizontal: 24),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Arta',
+                    style: TextStyle(color: Colors.grey, fontSize: 18),
+                  ),
+                  Text(
+                    'Rp${saldo ?? '0'}',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Card(
+            margin: EdgeInsets.symmetric(horizontal: 24),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'GoPay',
+                    style: TextStyle(color: Colors.grey, fontSize: 18),
+                  ),
+                  Text(
+                    'Rp150.000',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Card(
+            margin: EdgeInsets.symmetric(horizontal: 24),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'OVO',
+                    style: TextStyle(color: Colors.grey, fontSize: 18),
+                  ),
+                  Text(
+                    'Rp250.000',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 24),
+          SizedBox(
+            width: double.infinity,
+            child: FlatButton(
+              child: Text('+ Tambah Wallet'),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
     );
   }
-} 
+}
